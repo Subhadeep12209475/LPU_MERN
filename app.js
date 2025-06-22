54c0169369a0b39e222f7c7c3850739394cb32d2
Subhadeep Mukherjee <subhadeepmukherjee2019@gmail.com>
2025-06-22 23:36:28 +0530

    day 3

---
+++ app.js
@@ -0,0 +1,23 @@
+// const arr = ["cat","dog","elephant","tiger","lion"]
+
+// for(let i in arr){
+//   const ele = arr[i];
+//   console.log(ele,i)
+// }
+
+// for(let i of arr){
+//   console.log(i)
+// }
+
+
+// arr.forEach(myfun);
+
+// function myfun(i){
+//   let count=0;
+//   for(let i of arr){
+//     count++;
+//   }
+//   console.log(count);
+// }
+
+
